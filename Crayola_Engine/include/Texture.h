7cbea4dbd7deceb80e757d5517bb827e431b8b9d
Darkwarrior0417 <arturosf17@gmail.com>
2025-02-10 18:28:08 -0600

    Correction

--- Crayola_Engine/include/Texture.h
+++ Crayola_Engine/include/Texture.h
@@ -7,39 +7,39 @@ class DeviceContext;
 class Texture
 {
 public:
-    Texture() = default;
-    ~Texture() = default;
+	Texture() = default;
+	~Texture() = default;
 
-    HRESULT
-        init(Device device,
-            const std::string& textureName,
-            ExtensionType extensionType);
+	HRESULT
+		init(Device device,
+			const std::string& textureName,
+			ExtensionType extensionType);
 
-    HRESULT
-        init(Device device,
-            unsigned int width,
-            unsigned int height,
-            DXGI_FORMAT Format,
-            unsigned int BindFlags,
-            unsigned int sampleCount = 1,
-            unsigned int qualityLevels = 0);
+	HRESULT
+		init(Device device,
+			unsigned int width,
+			unsigned int height,
+			DXGI_FORMAT Format,
+			unsigned int BindFlags,
+			unsigned int sampleCount = 1,
+			unsigned int qualityLevels = 0);
 
-    void
-        update();
+	void
+		update();
 
-    void
-        render(DeviceContext& deviceContet,
-            unsigned int StartSlot,
-            unsigned int NumViews);
+	void
+		render(DeviceContext& deviceContet,
+			unsigned int StartSlot,
+			unsigned int NumViews);
 
-    void
-        destroy();
+	void
+		destroy();
 
 public:
-    // This variable is in charge of handle a texture resource as data
-    ID3D11Texture2D* m_texture = nullptr;
-    // This variable is in charge of handle a texture resource as image data
-    ID3D11ShaderResourceView* m_textureFromImg;
+	// This variable is in charge of handle a texture resource as data
+	ID3D11Texture2D* m_texture = nullptr;
+	// This variable is in charge of handle a texture resource as image data
+	ID3D11ShaderResourceView* m_textureFromImg;
 
 private:
 
